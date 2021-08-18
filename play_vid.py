039688c387c2ba58cb8ee5dae652dae6a234eed0
ocnow <ockulkarni97@gmail.com>
2021-08-18 10:02:44 +0530

    Defender Wall Mask Done

--- play_vid.py
+++ play_vid.py
@@ -4,8 +4,8 @@ import youtube_dl
 
 if __name__ == '__main__':
 
-    #video_url = 'https://www.youtube.com/watch?v=ld1Aw4wwNIM'
-    video_url = 'https://www.youtube.com/watch?v=GkzBDAaqiY4'
+    video_url = 'https://www.youtube.com/watch?v=ld1Aw4wwNIM'
+    #video_url = 'https://www.youtube.com/watch?v=GkzBDAaqiY4'
 
     ydl_opts = {}
 
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     for f in formats:
 
         # I want the lowest resolution, so I set resolution as 144p
-        if f.get('format_note',None) == '144p':
+        if f.get('format_note',None) == '1080p60':
 
             #get the video url
             url = f.get('url',None)
@@ -34,6 +34,8 @@ if __name__ == '__main__':
                 print('video not opened')
                 exit(-1)
 
+            cap.set(cv2.CAP_PROP_POS_MSEC,832000)
+
             while True:
                 # read frame
                 ret, frame = cap.read()
@@ -42,12 +44,27 @@ if __name__ == '__main__':
                 if not ret:
                     break
 
-                # display frame
-                cv2.imshow('frame', frame)
+                game_shape = frame.shape
+                map_frame = frame[0:int(game_shape[0]/2.4),0:game_shape[1]//4]
 
+                b,g,r = cv2.split(map_frame)
+                ret,g1 = cv2.threshold(g,177,255,cv2.THRESH_BINARY)
+                ret,b1 = cv2.threshold(b,190,255,cv2.THRESH_BINARY)
+                ret,r1 = cv2.threshold(r,152,255,cv2.THRESH_BINARY)
+
+                b_not = cv2.bitwise_not(b1)
+                r_not = cv2.bitwise_not(r1)
+
+                g1_bnot = cv2.bitwise_and(g1,b_not)
+                g1_brnot = cv2.bitwise_and(g1,r_not)
+
+                cv2.imshow('game',frame)
+                cv2.imshow('map',map_frame)
+                cv2.imshow('viperwall',g1_brnot)
                 if cv2.waitKey(30)&0xFF == ord('q'):
+                    #cv2.imwrite('map_frame_image.jpg',map_frame)
                     break
-
+            break
             # release VideoCapture
             cap.release()
 
