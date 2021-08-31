00007529931fc83d127de2d48fee41ff58bffc2a
ocnow <ockulkarni97@gmail.com>
2021-08-31 23:57:16 +0530

    mask done;moving to detect abilities

--- create_mask/create_mask.py
+++ create_mask/create_mask.py
@@ -3,60 +3,27 @@ import numpy as np
 
 currMap = cv2.imread('map_shot.jpg')
 
-res = cv2.Canny(currMap,100,200)
-
+res = cv2.Canny(currMap,30,200)
 frmShape = res.shape
+
+itemindex = np.where(res == 255)
+
+
+edge_pxls = []
+ru = np.unique(itemindex[0])
+for r in ru:
+    wh = np.where(itemindex[0] == r)[0]
+    edge_pxls.append((r,itemindex[1][wh[0]]))
+    edge_pxls.append((r,itemindex[1][wh[-1]]))
+
 blank_image = np.zeros((frmShape[0],frmShape[1]), np.uint8)
-blank_image1 = np.zeros((frmShape[0],frmShape[1]), np.uint8)
-blank_image2 = np.zeros((frmShape[0],frmShape[1]), np.uint8)
-blank_image3 = np.zeros((frmShape[0],frmShape[1]), np.uint8)
-print(blank_image.shape)
-print(frmShape)
-#make every pixel after first white as black
-for i in range(frmShape[0]):
-    foundWhite = False
-    for j in range(frmShape[1]):
-        if foundWhite:
-            blank_image[i][j] = 0
-        elif res[i][j] != 0:
-            blank_image[i][j] = 255
-            foundWhite = True
+for pxl in edge_pxls:
+    if(pxl[0] > 400):
+        break
+    for j in range(pxl[1],frmShape[1]):
+        blank_image[pxl[0],j] = 255 - blank_image[pxl[0],j]
 
-for i in range(frmShape[0]):
-    foundWhite = False
-    for j in range(frmShape[1]-1,-1,-1):
-        if foundWhite:
-            blank_image1[i][j] = 0
-        elif res[i][j] != 0:
-            blank_image1[i][j] = 255
-            foundWhite = True
-
-#make every pixel after first white as black
-for j in range(frmShape[1]):
-    foundWhite = False
-    for i in range(frmShape[0]):
-        if foundWhite:
-            blank_image2[i][j] = 0
-        elif res[i][j] != 0:
-            blank_image2[i][j] = 255
-            foundWhite = True
-
-for j in range(frmShape[1]):
-    foundWhite = False
-    for i in range(frmShape[0]-1,-1,-1):
-        if foundWhite:
-            blank_image3[i][j] = 0
-        elif res[i][j] != 0:
-            blank_image3[i][j] = 255
-            foundWhite = True
-
-
-res4 = cv2.bitwise_or(blank_image, blank_image1)
-res4 = cv2.bitwise_or(res4, blank_image2)
-res4 = cv2.bitwise_or(res4, blank_image3)
-
-cv2.imshow('wind1',currMap)
-cv2.imshow('wind2',res)
-cv2.imshow('wind3',res4)
-
+res4 = cv2.bitwise_and(currMap, currMap,mask = blank_image)
+cv2.imshow('mask_image.jpg',res4)
+#cv2.imwrite('mask_image.jpg',blank_image)
 cv2.waitKey(0)
